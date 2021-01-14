13191c5f9861af8acca8db6a7edbee48bc1cbecb
hussienail <63175073+hussienail@users.noreply.github.com>
2021-01-14 17:42:57 +0300

    Update fsdssfd.js

--- fsdssfd.js
+++ fsdssfd.js
@@ -25,10 +25,10 @@ $('.uzr.fl.corner.borderg').css({'margin': '0px 0',borderRight: '3px solid #005F
 $('.room.borderg.hand.nosel.fl').css({'margin': '1px 0',borderRight: '3px solid #005F69',borderLeft: '3px solid #adadad',borderTop: '1px solid #adadad',borderBottom: '1px solid #adadad',paddingTop: '3px',borderRadius: '5px'});
 $(`<div><marquee direction="right" width="100%" id="des1" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="5"> &nbsp;اهلا وسهلا بكم في شات <font color="#357524" class="des2"> نجمه الحب </font>افضل سوبر لهذا الاسبوع<font class="des2" color="#800000"> قريبـا </font>افضل زائر لهذا الاسبوع<font class="des2" color="#ff9900"> قريبـا </font>افضل زائرة لهذا الاسبوع<font class="des2" color="#ff00ff"> قريبـا </font>مبدع الحائط لهذا الاسبوع<font class="des2" color="#5CAD9A"> قريبـا </font>مبدعه الحائط لهذا الاسبوع<font class="des2" color="#800000"> قريبا َ</font>مع تمنياتنا للجميع باطيب الاوقات&nbsp;</marquee></div>`).insertBefore('div#tlogins .lonline.light.break'); 
 $(`<!--<a class="fl fa fa-image btn ui-corner-all ui-shadow ui-btn ui-btn-inline borderg" style="color:purple;margin:2px;width: 106px;text-align: center;font-size:15px!important;"><span dir="rtl"> (0) </span><span style="font-size: 15px!important;>omar alhaded </span></a>-->
-<a href="omar _ alhaded" target="_blank" style="width:107px; margin-right: 5px;
+<a href="ASIM" target="_blank" style="width:107px; margin-right: 5px;
 margin-left: 2px;text-align: center;float: right;border-radius: 5px;
 border-color: #990033;
-padding: 3px;font-family: 'Cairo', sans-serif, Fontawesome;font-size: 13px!important;" class="SLPBeats fab fa-angellist" >Style By:omar alhaded</a>`).insertBefore('a.fl.fa.fa-ban.btn.ui-corner-all.ui-shadow.ui-btn.ui-btn-inline.umute.borderg');
+padding: 3px;font-family: 'Cairo', sans-serif, Fontawesome;font-size: 13px!important;" class="SLPBeats fab fa-angellist" >Style By:ASIM</a>`).insertBefore('a.fl.fa.fa-ban.btn.ui-corner-all.ui-shadow.ui-btn.ui-btn-inline.umute.borderg');
 $("[data-target=#wall]").attr('title','  ♥️الحااائط للابـــــداع ♥️          ')
  $("a.fa.fa-user-plus").text(' Sign Up ');
  $("a.fa.fa-user").text(' Login ');
